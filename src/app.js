f4c21e525f6b1ae793034336a101e8e020457b61
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2021-06-18 19:29:55 -0300

    Get aula 1 exemplo

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/aula1', (req, res) => {
+  return res.json({
+    message: 'Colocar arquivo JSON com as aulas aqui',
+  });
+});
+
 app.listen(3000, () => {
   console.log('API running on port 3000');
 });
